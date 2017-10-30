d7be9380e5395c1b90475f90f5b839b434d5c79d
penguin56700 <penguin56700@gmail.com>
2017-10-30 22:08:57 +0800

    10.30
    刪除過舊版本
    HOUSE_MVC完成
    HOUSEIMAGE,MODEL完成

--- project/tzukai/sql/house~1.sql
+++
@@ -1,5 +0,0 @@
-insert into house (House_No,Title,Location,Price,Total_Pings,Main_Pings,Amenity_Pings,Accessory_Pings,Floorv,Age,Pattern,Orientation,Building_Materials,Parking_Space,Classification_Of_Land,Land_Pings,Data_Info,Main_Img,lat,LNG,House_States,Insert_Time)
-VALUES();
-
-insert into HOUSEIMAGES (IMG_NO,HOUSE_NO)
-VALUES ()
--- project1027/Cooper_Web-master/README.md
+++
@@ -1,4 +0,0 @@
-# Cooper_Web
-For_Web
-
-專門給Web技術的檔案
